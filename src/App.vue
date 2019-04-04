01e2d50e87589993c9bcbdac6c3b3bc0961a946d
Jess <jessica.sachs>
2019-04-04 11:19:14 -0400

    JS: WIP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+      <section class="section">
+        <h1 class="name">Jessica Sachs</h1>
+        <h2 class="job">Staff Engineer @ Intent</h2>
+        <p>A caffeine-based lifeform, circus nerd, VueJS afficionado, and VueVixen.</p>
+        <p>⚠️ Site under construction</p>
+        <section>
+          <h3>Links</h3>
+          <a href="https://twitter.com/_jessicasachs">Twitter is @_jessicasachs</a>
+          <a href="mailto:jess+web@jessicasachs.io">Email</a>
+        </section>
+
+
+        <!--<div class="activities">-->
+          <!--<div>☕ Caffeine-based lifeform</div>-->
+          <!--<div>🎪 Circus nerd</div>-->
+          <!--<div><img class="vue-spock" src="./assets/vue-spock.svg"/> VueJS afficionado</div>-->
+          <!--<div><img class="vue-vixen" src="./assets/vue-vixen.png"/> Vue Vixen</div>-->
+        <!--</div>-->
+      </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'normalize.css'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+  .job {
+    font-size: 2rem;
+  }
+  .name {
+    font-size: 4.242rem;
+    line-height: 4.5rem;
+    margin-top: 3rem;
+    font-weight: 700;
+  }
+
+
+
+
+
+  .section {
+    margin: 0 auto;
+}
+
+.vue-spock {
+    width: 1.1em;
+}
+
+.vue-vixen {
+    width: 1.1em;
+}
+
+.activities {
+    clear: both;
+    text-align: left;
+}
+
+.jess {
+    margin: 0 auto;
+    border-radius: 100%;
+    background-image: url(./assets/jess.jpg);
+    width: 100px;
+    height: 100px;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    box-shadow: 1px 2px 3px rgba(0,0,0,.4);
+}
+
+h1, h2, h3, h4, h5, h6, span {
+  margin: 0;
+}
+
+html, body {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+}
+
+  @media only screen and (max-width: 812px) {
+    body {
+      margin: 16px;
+    }
+  }
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    /*width: 800px;*/
+    margin: 100px auto 0 auto;
+  text-align: left;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,
+  Ubuntu, 'Helvetica Neue', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 1.3rem;
+
+  /*margin-top: 60px;*/
 }
 </style>
